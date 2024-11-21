f765f781f14ea559fdbccbdebbd9e4c6b1aad41c
Erin Mikaela Esteban <mikaella03@gmail.com>
2024-11-21 10:42:17 +0800

    update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,23 @@
 const hamburgerNav = document.querySelector('.hamburger-nav');
 const navMenu = document.querySelector('.nav-menu');
 
-hamburgerNav.addEventListener('click', () => {
-  hamburgerNav.classList.toggle('active');
-  navMenu.classList.toggle('active');
-})
+document.addEventListener('DOMContentLoaded', function() {
+  hamburgerNav.addEventListener('click', function() {
+    hamburgerNav.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+});
 
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-addEventListener('click', () => {
   hamburgerNav.classList.remove('active');
   navMenu.classList.remove('active');
-})
-}))
+}));
 
 document.addEventListener('scroll', () => {
-const header = document.querySelector('header');
-
-if (window.scrollY > 0) {
-  header.classList.add('scrolled');
-} else {
-  header.classList.remove('scrolled');
-}
-})
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
